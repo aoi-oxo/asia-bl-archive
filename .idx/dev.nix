7188983dcb1df6114955eb3787093f8a2c7feb26
mauve.azur <mauve.azur@gmail.com>
2026-02-16 16:21:58 +0000

    ページネーション追加

---
+++ .idx/dev.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }: {
+  channel = "stable-24.05";
+  packages = [ pkgs.python3 ];
+  idx = {
+    extensions = [ "vscodevim.vim" ];
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
